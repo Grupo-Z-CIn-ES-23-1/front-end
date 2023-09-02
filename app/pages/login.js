4ecc897848f753e638794280bbd0a89e0738cb29
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-09-02 02:44:07 -0300

    feat: added simple login page

---
+++ app/pages/login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Router from "next/router";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:80/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      if (data.token) {
+        // Aqui, usamos o localStorage, mas você pode optar por cookies.
+        localStorage.setItem("jwt", data.token);
+        Router.push("/dashboard"); // Redireciona para o painel após o login.
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("Erro no login:", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
