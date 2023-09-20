fd6e2ea6b2d2c1c997270c834a3993cc8233782c
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-09-20 02:54:39 -0300

    refactor: fixing some build warnings on index.js

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
-function index() {
+function Index() {
   const router = useRouter();
 
   useEffect(() => {
     router.push("/dashboard");
-  }, []);
+  }, [router]);
 
   return null; // Poderia retornar um fallback
 }
 
-export default index;
+export default Index;
